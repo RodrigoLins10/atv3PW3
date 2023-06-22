fab5355db74b670f144f4d1fed2b4bd6eeef0a6f
´´Evilyn-rms` <rms.n.evil@gmail.com`>
2023-06-22 17:02:56 -0300

    commit2.0

--- Take_In/routes/web.php
+++ Take_In/routes/web.php
@@ -16,4 +16,18 @@ Route::get('/takein', [TakeinController::class, 'inicial']);
 Route::get('/takein/signin', [SignInController::class]);
 Route::get('/takein/singup', [SignUpController::class]);
 Route::get('/takein/home',[HomeController::class]);
-Route::get('/takein/userpage', [UserController::class]);
+Route::get('/takein/userpage', [UserController::class]); 
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+
+Route::get('/incomes/create', [IncomeController::class, 'create'])->name('incomes.create');
+Route::post('/incomes', [IncomeController::class, 'store'])->name('incomes.store');
